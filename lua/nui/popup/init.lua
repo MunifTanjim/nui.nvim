f4134943b65c00d94d5f468340d2a5e3b401a12d
Yago Riveiro <yago.riveiro@gmail.com>
2021-11-07 12:39:13 +0000

    fix: wrong logical operation

--- lua/nui/popup/init.lua
+++ lua/nui/popup/init.lua
@@ -277,7 +277,7 @@ function Popup:hide()
 end
 
 function Popup:show()
-  if self.popup_state.loading or self.popup_state.mounted then
+  if self.popup_state.loading or not self.popup_state.mounted then
     return
   end
 
