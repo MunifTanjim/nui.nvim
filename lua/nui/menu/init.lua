a69d1e060437c16a63761be3f76097227a333b41
Munif Tanjim <hello@muniftanjim.dev>
2021-12-03 15:24:02 +0600

    refactor(menu): cleanup implementation and use nui.tree

--- lua/nui/menu/init.lua
+++ lua/nui/menu/init.lua
@@ -1,66 +1,31 @@
-local _utils = require("nui.utils")._
+local _ = require("nui.utils")._
 local defaults = require("nui.utils").defaults
 local is_type = require("nui.utils").is_type
-local event = require("nui.utils.autocmd").event
 local Popup = require("nui.popup")
+local Tree = require("nui.tree")
 
-local function parse_lines(lines)
+local function prepare_lines(lines)
   local data = {
     lines = {},
-    total_lines = 0,
-    max_line_length = 0,
+    _max_line_width = 0,
   }
 
   for index, line in ipairs(lines) do
-    data.total_lines = data.total_lines + 1
-
     line._index = index
 
     if line.type == "item" then
-      table.insert(data.lines, line)
-
       local line_length = vim.api.nvim_strwidth(line.text)
-      if data.max_line_length < line_length then
-        data.max_line_length = line_length
+      if data._max_line_width < line_length then
+        data._max_line_width = line_length
       end
+
+      data.lines[index] = line
     elseif line.type == "separator" then
-      table.insert(data.lines, line)
+      data.lines[index] = line
     end
   end
 
   return data
-end
-
-local function calculate_buf_lines(menu)
-  local buf_lines = {}
-
-  local border_props = menu.border.border_props
-
-  local default_char = is_type("table", border_props.char) and border_props.char.top or ""
-  local default_text_align = is_type("table", border_props.text) and border_props.text.top_align or "left"
-
-  local separator_char = defaults(menu.menu_props.separator.char, default_char)
-  local separator_text_align = defaults(menu.menu_props.separator.text_align, default_text_align)
-
-  local max_length = menu.popup_props.size.width
-  local separator_max_length = max_length - vim.api.nvim_strwidth(separator_char) * 2
-
-  for _index, line in ipairs(menu.menu_props.lines) do
-    if line.type == "item" then
-      local text = _utils.truncate_text(line.text, max_length)
-      table.insert(buf_lines, text)
-    elseif line.type == "separator" then
-      local text = _utils.align_text(
-        _utils.truncate_text(line.text, separator_max_length),
-        separator_text_align,
-        separator_max_length,
-        separator_char
-      )
-      table.insert(buf_lines, separator_char .. text .. separator_char)
-    end
-  end
-
-  return buf_lines
 end
 
 local default_keymap = {
@@ -85,36 +50,39 @@ local function parse_keymap(keymap)
 end
 
 ---@param direction "'next'" | "'prev'"
----@param current_index nil | number
-local function focus_item(menu, direction, current_index)
+---@param current_id nil | number
+local function focus_item(menu, direction, current_id)
   if not menu.popup_state.mounted then
     return
   end
 
-  local curr_index = defaults(current_index, menu.menu_state.curr_index)
+  local curr_node = menu._tree:get_node(current_id)
+  local curr_id = curr_node:get_id()
 
-  local next_index = nil
+  local next_id = nil
 
   if direction == "next" then
-    if curr_index == menu.menu_props.total_lines then
-      next_index = 1
+    if curr_id == #menu._tree.nodes.root_ids then
+      next_id = 1
     else
-      next_index = curr_index + 1
+      next_id = curr_id + 1
     end
   elseif direction == "prev" then
-    if curr_index == 1 then
-      next_index = menu.menu_props.total_lines
+    if curr_id == 1 then
+      next_id = #menu._tree.nodes.root_ids
     else
-      next_index = curr_index - 1
+      next_id = curr_id - 1
     end
   end
 
-  if menu.menu_props.lines[next_index].type == "separator" then
-    return focus_item(menu, direction, next_index)
+  local next_node = menu._tree:get_node(next_id)
+
+  if next_node.type == "separator" then
+    return focus_item(menu, direction, next_id)
   end
 
-  if next_index then
-    vim.api.nvim_win_set_cursor(menu.winid, { next_index, 0 })
+  if next_id then
+    vim.api.nvim_win_set_cursor(menu.winid, { next_id, 0 })
   end
 end
 
@@ -122,27 +90,13 @@ local function init(class, popup_options, options)
   local props = vim.tbl_extend("force", {
     separator = defaults(options.separator, {}),
     keymap = parse_keymap(options.keymap),
-  }, parse_lines(options.lines))
-
-  local state = {
-    curr_index = nil,
-  }
-
-  for _, line in ipairs(props.lines) do
-    if line.type == "item" then
-      state.curr_index = line._index
-      break
-    end
-  end
+  }, prepare_lines(options.lines))
 
   local width = math.max(
-    math.min(props.max_line_length, defaults(options.max_width, 999)),
+    math.min(props._max_line_width, defaults(options.max_width, 999)),
     defaults(options.min_width, 16)
   )
-  local height = math.max(
-    math.min(props.total_lines, defaults(options.max_height, 999)),
-    defaults(options.min_height, 1)
-  )
+  local height = math.max(math.min(#props.lines, defaults(options.max_height, 999)), defaults(options.min_height, 1))
 
   popup_options = vim.tbl_deep_extend("force", {
     enter = true,
@@ -160,13 +114,9 @@ local function init(class, popup_options, options)
   local self = class.super.init(class, popup_options)
 
   self.menu_props = props
-  self.menu_state = state
-
-  props.buf_lines = calculate_buf_lines(self)
 
   props.on_submit = function()
-    local curr_index = self.menu_state.curr_index
-    local item = self.menu_props.lines[curr_index]
+    local item = self._tree:get_node()
 
     self:unmount()
 
@@ -203,10 +153,10 @@ local Menu = setmetatable({
 
 ---@param text nil | string
 function Menu.separator(text)
-  return {
+  return Tree.Node({
     type = "separator",
     text = defaults(text, ""),
-  }
+  })
 end
 
 ---@param item string | table
@@ -220,22 +170,47 @@ function Menu.item(item, props)
 
   object.type = "item"
 
-  return object
+  return Tree.Node(object)
 end
 
 function Menu:init(popup_options, options)
   return init(self, popup_options, options)
 end
 
+local function make_prepare_node(menu)
+  local props = menu.menu_props
+  local popup_props = menu.popup_props
+  local border_props = menu.border.border_props
+
+  local default_char = is_type("table", border_props.char) and border_props.char.top or ""
+  local default_text_align = is_type("table", border_props.text) and border_props.text.top_align or "left"
+
+  local separator_char = defaults(props.separator.char, default_char)
+  local separator_text_align = defaults(props.separator.text_align, default_text_align)
+
+  local max_length = popup_props.size.width
+  local separator_max_length = max_length - vim.api.nvim_strwidth(separator_char) * 2
+
+  return function(node)
+    if node.type == "item" then
+      local text = _.truncate_text(node.text, max_length)
+      return text
+    elseif node.type == "separator" then
+      local text = _.align_text(
+        _.truncate_text(node.text, separator_max_length),
+        separator_text_align,
+        separator_max_length,
+        separator_char
+      )
+      return separator_char .. text .. separator_char
+    end
+  end
+end
+
 function Menu:mount()
   self.super.mount(self)
 
   local props = self.menu_props
-
-  self:on(event.CursorMoved, function()
-    local index = vim.api.nvim_win_get_cursor(self.winid)[1]
-    self.menu_state.curr_index = index
-  end, {})
 
   for _, key in pairs(props.keymap.focus_next) do
     self:map("n", key, props.on_focus_next, { noremap = true, nowait = true })
@@ -253,10 +228,25 @@ function Menu:mount()
     self:map("n", key, props.on_submit, { noremap = true, nowait = true })
   end
 
-  vim.api.nvim_buf_set_lines(self.bufnr, 0, #self.menu_props.buf_lines, false, self.menu_props.buf_lines)
-  vim.api.nvim_buf_set_option(self.bufnr, "readonly", true)
-  vim.api.nvim_buf_set_option(self.bufnr, "modifiable", false)
-  vim.api.nvim_win_set_cursor(self.winid, { self.menu_state.curr_index, 0 })
+  self._tree = Tree({
+    winid = self.winid,
+    nodes = self.menu_props.lines,
+    get_node_id = function(node)
+      return node._index
+    end,
+    prepare_node = make_prepare_node(self),
+  })
+
+  self._tree:render()
+
+  -- jump to first item
+  for _, node_id in ipairs(self._tree.nodes.root_ids) do
+    local node = self._tree:get_node(node_id)
+    if node.type == "item" then
+      vim.api.nvim_win_set_cursor(self.winid, { node_id, 0 })
+      break
+    end
+  end
 end
 
 local MenuClass = setmetatable({
