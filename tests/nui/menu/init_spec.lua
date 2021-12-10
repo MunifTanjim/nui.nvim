ce53dd29e96fa5d6c33f120926bd2f0d0239d259
Munif Tanjim <hello@muniftanjim.dev>
2021-12-10 18:09:23 +0600

    test(menu): add tests for menu item

--- tests/nui/menu/init_spec.lua
+++ tests/nui/menu/init_spec.lua
@@ -1,8 +1,9 @@
 local Menu = require("nui.menu")
+local Text = require("nui.text")
 local helper = require("tests.nui")
 local spy = require("luassert.spy")
 
-local eq, feedkeys = helper.eq, helper.feedkeys
+local eq, feedkeys, tbl_pick = helper.eq, helper.feedkeys, helper.tbl_pick
 
 describe("nui.menu", function()
   local callbacks
@@ -139,5 +140,117 @@ describe("nui.menu", function()
     feedkeys("k", "x")
     assert.spy(on_change).called_with(lines[2], menu)
     on_change:clear()
+  end)
+
+  describe("item", function()
+    it("is prepared using o.prepare_item if provided", function()
+      local items = {
+        Menu.item("A"),
+        Menu.separator("*"),
+        Menu.item("B"),
+      }
+
+      local function prepare_item(item)
+        return "-" .. item.text .. "-"
+      end
+
+      local menu = Menu(popup_options, {
+        lines = items,
+        prepare_item = prepare_item,
+      })
+
+      menu:mount()
+
+      eq(vim.api.nvim_buf_get_lines(menu.bufnr, 0, -1, false), vim.tbl_map(prepare_item, items))
+    end)
+
+    it("is prepared when o.prepare_item is not provided", function()
+      local items = {
+        Menu.item("A"),
+        Menu.separator("*"),
+        Menu.item("B"),
+      }
+
+      popup_options.border = "single"
+
+      local menu = Menu(popup_options, {
+        lines = items,
+      })
+
+      menu:mount()
+
+      eq(vim.api.nvim_buf_get_lines(menu.bufnr, 0, -1, false), {
+        "A",
+        "─*──",
+        "B",
+      })
+    end)
+
+    it("is skipped respecting o.should_skip_item if provided", function()
+      local on_change = spy.on(callbacks, "on_change")
+
+      local items = {
+        Menu.item("-"),
+        Menu.item("A", { id = 1 }),
+        Menu.item("-"),
+        Menu.item("B", { id = 2 }),
+        Menu.item("-"),
+      }
+
+      local menu = Menu(popup_options, {
+        lines = items,
+        on_change = on_change,
+        should_skip_item = function(item)
+          return not item.id
+        end,
+      })
+
+      menu:mount()
+
+      assert.spy(on_change).called_with(items[2], menu)
+      on_change:clear()
+
+      feedkeys("j", "x")
+      assert.spy(on_change).called_with(items[4], menu)
+      on_change:clear()
+
+      feedkeys("j", "x")
+      assert.spy(on_change).called_with(items[2], menu)
+      on_change:clear()
+    end)
+
+    it("supports NuiText", function()
+      local hl_group = "NuiMenuTest"
+      local text = "text"
+      local items = {
+        Menu.item(Text(text, hl_group)),
+      }
+
+      local menu = Menu(popup_options, {
+        lines = items,
+      })
+
+      menu:mount()
+
+      eq(vim.api.nvim_buf_get_lines(menu.bufnr, 0, -1, false), { text })
+
+      local linenr = 1
+      local line = vim.api.nvim_buf_get_lines(menu.bufnr, linenr - 1, linenr, false)[linenr]
+      local byte_start = string.find(line, text)
+
+      local extmarks = vim.api.nvim_buf_get_extmarks(menu.bufnr, menu.ns_id, linenr - 1, linenr, {
+        details = true,
+      })
+
+      eq(type(byte_start), "number")
+
+      eq(#extmarks, 1)
+      eq(extmarks[1][2], linenr - 1)
+      eq(extmarks[1][4].end_col - extmarks[1][3], #text)
+      eq(tbl_pick(extmarks[1][4], { "end_row", "hl_group" }), {
+        end_row = linenr - 1,
+        hl_group = hl_group,
+      })
+    end)
   end)
 end)
