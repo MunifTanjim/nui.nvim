c7622f9e95db6996f31c2062db0dfcc6796a489f
Munif Tanjim <hello@muniftanjim.dev>
2021-12-10 13:07:35 +0600

    chore: update scripts/format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env sh
 
-expected_stylua_version="0.11.2"
-stylua_version="$(stylua --version | cut -d' ' -f2)"
+r_v_major="0"
+r_v_minor="11"
+r_v_patch="2"
 
-if test "${stylua_version}" != "${expected_stylua_version}"; then
-  echo "expected stylua v${expected_stylua_version}, found v${stylua_version}"
+v="$(stylua --version | cut -d' ' -f2)"
+v_major="$(echo "${v}" | cut -d'.' -f1)"
+v_minor="$(echo "${v}" | cut -d'.' -f2)"
+v_patch="$(echo "${v}" | cut -d'.' -f3)"
+
+v_error_message="required stylua >= v${r_v_major}.${r_v_minor}.${r_v_patch}, found v${v_major}.${v_minor}.${v_patch}"
+
+if test ${v_major} -ne ${r_v_major} || test ${v_minor} -lt ${r_v_minor} || test ${v_patch} -lt ${r_v_patch}; then
+  echo ${v_error_message} >&2
   exit 1
 fi
 
-stylua --color always --check lua/
+stylua --color always ${1} lua/nui/ lua/tests/nui/ tests/nui/
