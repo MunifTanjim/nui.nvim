1831b832c7669b4ef97ff09f7576fa6076171d03
Munif Tanjim <hello@muniftanjim.dev>
2021-12-31 04:58:33 +0600

    test: add buf_lines and extmark assert helper

--- lua/tests/nui/init.lua
+++ lua/tests/nui/init.lua
@@ -8,6 +8,8 @@ function mod.feedkeys(keys, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), mode or "", true)
 end
 
+---@param tbl table
+---@param keys string[]
 function mod.tbl_pick(tbl, keys)
   if not keys or #keys == 0 then
     return tbl
@@ -20,6 +22,8 @@ function mod.tbl_pick(tbl, keys)
   return new_tbl
 end
 
+---@param tbl table
+---@param keys string[]
 function mod.tbl_omit(tbl, keys)
   if not keys or #keys == 0 then
     return tbl
@@ -30,6 +34,27 @@ function mod.tbl_omit(tbl, keys)
     rawset(new_tbl, key, nil)
   end
   return new_tbl
+end
+
+---@param bufnr number
+---@param lines string[]
+---@param linenr_start? number
+---@param linenr_end? number
+function mod.assert_buf_lines(bufnr, lines, linenr_start, linenr_end)
+  mod.eq(vim.api.nvim_buf_get_lines(bufnr, linenr_start or 0, linenr_end or -1, false), lines)
+end
+
+---@param extmark table
+---@param linenr number
+---@param text string
+---@param hl_group string
+function mod.assert_extmark(extmark, linenr, text, hl_group)
+  mod.eq(extmark[2], linenr - 1)
+  mod.eq(extmark[4].end_col - extmark[3], #text)
+  mod.eq(mod.tbl_pick(extmark[4], { "end_row", "hl_group" }), {
+    end_row = linenr - 1,
+    hl_group = hl_group,
+  })
 end
 
 return mod
