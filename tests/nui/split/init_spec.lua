cdaf49c67c8ed54e4bd0bada7977a1fc2eaae0c3
Munif Tanjim <hello@muniftanjim.dev>
2022-01-24 01:03:06 +0600

    test(split): add test for method split:map

--- tests/nui/split/init_spec.lua
+++ tests/nui/split/init_spec.lua
@@ -13,6 +13,101 @@ describe("nui.split", function()
     split:unmount()
   end)
 
+  describe("method :map", function()
+    it("supports lhs table", function()
+      split = Split({
+        size = 20,
+      })
+
+      split:mount()
+
+      split:map("n", { "k", "l" }, "o42<esc>")
+
+      feedkeys("k", "x")
+      feedkeys("l", "x")
+
+      h.assert_buf_lines(split.bufnr, {
+        "",
+        "42",
+        "42",
+      })
+    end)
+
+    it("supports rhs function", function()
+      local callback = spy.new(function() end)
+
+      split = Split({
+        size = 20,
+      })
+
+      split:mount()
+
+      split:map("n", "l", function()
+        callback()
+      end)
+
+      feedkeys("l", "x")
+
+      assert.spy(callback).called()
+    end)
+
+    it("supports rhs string", function()
+      split = Split({
+        size = 20,
+      })
+
+      split:mount()
+
+      split:map("n", "l", "o42<esc>")
+
+      feedkeys("l", "x")
+
+      h.assert_buf_lines(split.bufnr, {
+        "",
+        "42",
+      })
+    end)
+
+    it("supports o.remap=true", function()
+      split = Split({
+        size = 20,
+      })
+
+      split:mount()
+
+      split:map("n", "k", "o42<Esc>")
+      split:map("n", "l", "k", { remap = true })
+
+      feedkeys("k", "x")
+      feedkeys("l", "x")
+
+      h.assert_buf_lines(split.bufnr, {
+        "",
+        "42",
+        "42",
+      })
+    end)
+
+    it("supports o.remap=false", function()
+      split = Split({
+        size = 20,
+      })
+
+      split:mount()
+
+      split:map("n", "k", "o42<Esc>")
+      split:map("n", "l", "k", { remap = false })
+
+      feedkeys("k", "x")
+      feedkeys("l", "x")
+
+      h.assert_buf_lines(split.bufnr, {
+        "",
+        "42",
+      })
+    end)
+  end)
+
   describe("method :unmap", function()
     it("supports lhs string", function()
       split = Split({
