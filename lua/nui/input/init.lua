6f803e88093573f73d4ee6c0dfe0575df3f97a9f
Munif Tanjim <hello@muniftanjim.dev>
2022-01-30 17:50:02 +0600

    fix(input): try to keep stable cursor position on parent window

--- lua/nui/input/init.lua
+++ lua/nui/input/init.lua
@@ -4,6 +4,23 @@ local defaults = require("nui.utils").defaults
 local is_type = require("nui.utils").is_type
 local event = require("nui.utils.autocmd").event
 
+-- exiting insert mode places cursor one character backward,
+-- so patch the cursor position to one character forward
+-- when unmounting input.
+---@param target_cursor number[]
+---@param force? boolean
+local function patch_cursor_position(target_cursor, force)
+  local cursor = vim.api.nvim_win_get_cursor(0)
+
+  if target_cursor[2] == cursor[2] and force then
+    -- didn't exit insert mode yet, but it's gonna
+    vim.api.nvim_win_set_cursor(0, { cursor[1], cursor[2] + 1 })
+  elseif target_cursor[2] - 1 == cursor[2] then
+    -- already exited insert mode
+    vim.api.nvim_win_set_cursor(0, { cursor[1], cursor[2] + 1 })
+  end
+end
+
 ---@param class NuiInput
 ---@param popup_options table
 ---@param options table
@@ -27,12 +44,15 @@ local function init(class, popup_options, options)
 
   self._.default_value = defaults(options.default_value, "")
   self._.prompt = Text(defaults(options.prompt, ""))
+  self._.disable_cursor_position_patch = defaults(options.disable_cursor_position_patch, false)
 
   local props = {}
 
   self.input_props = props
 
   props.on_submit = function(value)
+    local target_cursor = vim.api.nvim_win_get_cursor(self._.position.win)
+
     local prompt_normal_mode = vim.fn.mode() == "n"
 
     self:unmount()
@@ -44,6 +64,10 @@ local function init(class, popup_options, options)
         vim.api.nvim_command("stopinsert")
       end
 
+      if not self._.disable_cursor_position_patch then
+        patch_cursor_position(target_cursor, prompt_normal_mode)
+      end
+
       if options.on_submit then
         options.on_submit(value)
       end
@@ -51,11 +75,17 @@ local function init(class, popup_options, options)
   end
 
   props.on_close = function()
+    local target_cursor = vim.api.nvim_win_get_cursor(self._.position.win)
+
     self:unmount()
 
     vim.schedule(function()
       if vim.fn.mode() == "i" then
         vim.api.nvim_command("stopinsert")
+      end
+
+      if not self._.disable_cursor_position_patch then
+        patch_cursor_position(target_cursor)
       end
 
       if options.on_close then
--- tests/nui/input/init_spec.lua
+++ tests/nui/input/init_spec.lua
@@ -2,17 +2,21 @@ pcall(require, "luacov")
 
 local Input = require("nui.input")
 local Text = require("nui.text")
-local helper = require("tests.nui")
+local h = require("tests.nui")
 
-local eq, tbl_pick = helper.eq, helper.tbl_pick
+local eq, feedkeys, tbl_pick = h.eq, h.feedkeys, h.tbl_pick
 
 -- Input's functionalities are not testable using headless nvim.
 -- Not sure what to do about it.
 
-pending("nui.input", function()
+describe("nui.input", function()
+  local parent_winid, parent_bufnr
   local popup_options
 
   before_each(function()
+    parent_winid = vim.api.nvim_get_current_win()
+    parent_bufnr = vim.api.nvim_get_current_buf()
+
     popup_options = {
       relative = "win",
       position = "50%",
@@ -20,7 +24,7 @@ pending("nui.input", function()
     }
   end)
 
-  describe("o.prompt", function()
+  pending("o.prompt", function()
     it("supports NuiText", function()
       local prompt_text = "> "
       local hl_group = "NuiInputTest"
@@ -50,6 +54,112 @@ pending("nui.input", function()
         end_row = linenr - 1,
         hl_group = hl_group,
       })
+    end)
+  end)
+
+  describe("cursor_position_patch", function()
+    local initial_cursor
+
+    local function setup()
+      vim.api.nvim_buf_set_lines(parent_bufnr, 0, -1, false, {
+        "1 nui.nvim",
+        "2 nui.nvim",
+        "3 nui.nvim",
+      })
+      initial_cursor = { 2, 4 }
+      vim.api.nvim_win_set_cursor(parent_winid, initial_cursor)
+    end
+
+    it("works after submitting from insert mode", function()
+      setup()
+
+      local done = false
+      local input = Input(popup_options, {
+        on_submit = function()
+          done = true
+        end,
+      })
+
+      input:mount()
+
+      feedkeys("<cr>", "x")
+
+      vim.fn.wait(1000, function()
+        return done
+      end)
+
+      eq(done, true)
+      eq(vim.api.nvim_win_get_cursor(parent_winid), initial_cursor)
+    end)
+
+    it("works after submitting from normal mode", function()
+      setup()
+
+      local done = false
+      local input = Input(popup_options, {
+        on_submit = function()
+          done = true
+        end,
+      })
+
+      input:mount()
+
+      feedkeys("<esc><cr>", "x")
+
+      vim.fn.wait(1000, function()
+        return done
+      end)
+
+      eq(done, true)
+      eq(vim.api.nvim_win_get_cursor(parent_winid), initial_cursor)
+    end)
+
+    it("works after closing from insert mode", function()
+      setup()
+
+      local done = false
+      local input = Input(popup_options, {
+        on_close = function()
+          done = true
+        end,
+      })
+
+      input:mount()
+
+      input:map("i", "<esc>", input.input_props.on_close, { nowait = true, noremap = true })
+
+      feedkeys("i<esc>", "x")
+
+      vim.fn.wait(1000, function()
+        return done
+      end)
+
+      eq(done, true)
+      eq(vim.api.nvim_win_get_cursor(parent_winid), initial_cursor)
+    end)
+
+    it("works after closing from normal mode", function()
+      setup()
+
+      local done = false
+      local input = Input(popup_options, {
+        on_close = function()
+          done = true
+        end,
+      })
+
+      input:mount()
+
+      input:map("n", "<esc>", input.input_props.on_close, { nowait = true, noremap = true })
+
+      feedkeys("<esc>", "x")
+
+      vim.fn.wait(1000, function()
+        return done
+      end)
+
+      eq(done, true)
+      eq(vim.api.nvim_win_get_cursor(parent_winid), initial_cursor)
     end)
   end)
 end)
