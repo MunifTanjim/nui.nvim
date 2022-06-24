0ee21308571c17c75f1f556845122328be7c08a2
Munif Tanjim <hello@muniftanjim.dev>
2022-06-24 13:07:00 +0600

    test(layout): add tests for o.size

--- tests/nui/layout/init_spec.lua
+++ tests/nui/layout/init_spec.lua
@@ -5,7 +5,7 @@ local Popup = require("nui.popup")
 local h = require("tests.nui")
 local spy = require("luassert.spy")
 
-local eq = h.eq
+local eq, tbl_pick = h.eq, h.tbl_pick
 
 local function create_popups(...)
   local popups = {}
@@ -20,6 +20,73 @@ describe("nui.layout", function()
 
   after_each(function()
     layout:unmount()
+  end)
+
+  describe("o.size", function()
+    local function assert_size(size)
+      local win_config = vim.api.nvim_win_get_config(layout.winid)
+
+      eq(tbl_pick(win_config, { "width", "height" }), {
+        width = math.floor(size.width),
+        height = math.floor(size.height),
+      })
+    end
+
+    it("supports number", function()
+      local size = 20
+
+      layout = Layout({
+        position = "50%",
+        size = size,
+      }, {})
+
+      layout:mount()
+
+      assert_size({ width = size, height = size })
+    end)
+
+    it("supports percentage string", function()
+      local percentage = 50
+
+      layout = Layout({
+        position = "50%",
+        size = string.format("%s%%", percentage),
+      }, {})
+
+      local winid = vim.api.nvim_get_current_win()
+      local win_width = vim.api.nvim_win_get_width(winid)
+      local win_height = vim.api.nvim_win_get_height(winid)
+
+      layout:mount()
+
+      assert_size({
+        width = win_width * percentage / 100,
+        height = win_height * percentage / 100,
+      })
+    end)
+
+    it("supports table", function()
+      local width = 10
+      local height_percentage = 50
+
+      layout = Layout({
+        position = "50%",
+        size = {
+          width = width,
+          height = string.format("%s%%", height_percentage),
+        },
+      }, {})
+
+      local winid = vim.api.nvim_get_current_win()
+      local win_height = vim.api.nvim_win_get_height(winid)
+
+      layout:mount()
+
+      assert_size({
+        width = width,
+        height = win_height * height_percentage / 100,
+      })
+    end)
   end)
 
   describe("method :mount", function()
