a5fd005263d238d2fbd6ee335e06139645f11fa9
Munif Tanjim <hello@muniftanjim.dev>
2022-07-31 20:29:17 +0600

    feat(layout): introduce layout type

--- lua/nui/layout/init.lua
+++ lua/nui/layout/init.lua
@@ -1,4 +1,5 @@
 local Object = require("nui.object")
+local Popup = require("nui.popup")
 local utils = require("nui.utils")
 local layout_utils = require("nui.layout.utils")
 local float_layout = require("nui.layout.float")
@@ -54,70 +55,84 @@ local function get_layout_config_relative_to_component(component)
   }
 end
 
-local function is_box_empty(box)
-  for _, child in ipairs(box.box) do
-    if child.component then
-      return false
-    end
-    if not is_box_empty(child) then
-      return false
-    end
-  end
-  return true
-end
-
 ---@class NuiLayout
 local Layout = Object("NuiLayout")
+
+---@return '"float"' layout_type
+local function get_layout_type(box)
+  for _, child in ipairs(box.box) do
+    if child.component and child.type then
+      return child.type
+    end
+
+    local type = get_layout_type(child)
+    if type then
+      return type
+    end
+  end
+
+  error("unexpected empty box")
+end
 
 function Layout:init(options, box)
   box = Layout.Box(box)
 
-  if is_box_empty(box) then
-    error("unexpected empty box")
-  end
-
-  local container
-  if is_component(options) then
-    container = options
-    options = get_layout_config_relative_to_component(container)
-  else
-    options = merge_default_options(options)
-    options = normalize_options(options)
-  end
+  local type = get_layout_type(box)
 
   self._ = {
+    type = type,
     box = box,
-    container = container,
-    layout = {},
     loading = false,
     mounted = false,
-    win_enter = false,
-    win_config = {
-      focusable = false,
-      style = "minimal",
-      zindex = 49,
-    },
-    win_options = {
-      winblend = 100,
-    },
   }
 
-  if not is_component(container) or is_component_mounted(container) then
-    self:update(options)
+  if type == "float" then
+    local container
+    if is_component(options) then
+      container = options
+      options = get_layout_config_relative_to_component(container)
+    else
+      options = merge_default_options(options)
+      options = normalize_options(options)
+    end
+
+    self._[type] = {
+      container = container,
+      layout = {},
+      win_enter = false,
+      win_config = {
+        focusable = false,
+        style = "minimal",
+        zindex = 49,
+      },
+      win_options = {
+        winblend = 100,
+      },
+    }
+
+    if not is_component(container) or is_component_mounted(container) then
+      self:update(options)
+    end
   end
 end
 
 function Layout:_process_layout()
-  apply_workaround_for_float_relative_position_issue_18925(self)
+  local type = self._.type
 
-  float_layout.process(self._.box, {
-    winid = self.winid,
-    container_size = self._.size,
-    position = {
-      row = 0,
-      col = 0,
-    },
-  })
+  if type == "float" then
+    local info = self._.float
+
+    apply_workaround_for_float_relative_position_issue_18925(self)
+
+    float_layout.process(self._.box, {
+      winid = self.winid,
+      container_size = info.size,
+      position = {
+        row = 0,
+        col = 0,
+      },
+    })
+  end
 end
 
 function Layout:mount()
@@ -127,25 +142,33 @@ function Layout:mount()
 
   self._.loading = true
 
-  local container = self._.container
-  if is_component(container) and not is_component_mounted(container) then
-    container:mount()
-    self:update(get_layout_config_relative_to_component(container))
+  local type = self._.type
+
+  if type == "float" then
+    local info = self._.float
+
+    local container = info.container
+    if is_component(container) and not is_component_mounted(container) then
+      container:mount()
+      self:update(get_layout_config_relative_to_component(container))
+    end
+
+    if not self.bufnr then
+      self.bufnr = vim.api.nvim_create_buf(false, true)
+      assert(self.bufnr, "failed to create buffer")
+    end
+
+    self.winid = vim.api.nvim_open_win(self.bufnr, info.win_enter, info.win_config)
+    assert(self.winid, "failed to create popup window")
+
+    _.set_win_options(self.winid, info.win_options)
   end
-
-  if not self.bufnr then
-    self.bufnr = vim.api.nvim_create_buf(false, true)
-    assert(self.bufnr, "failed to create buffer")
-  end
-
-  self.winid = vim.api.nvim_open_win(self.bufnr, self._.win_enter, self._.win_config)
-  assert(self.winid, "failed to create popup window")
-
-  _.set_win_options(self.winid, self._.win_options)
 
   self:_process_layout()
 
-  float_layout.mount_box(self._.box)
+  if type == "float" then
+    float_layout.mount_box(self._.box)
+  end
 
   self._.loading = false
   self._.mounted = true
@@ -158,19 +181,23 @@ function Layout:unmount()
 
   self._.loading = true
 
-  float_layout.unmount_box(self._.box)
+  local type = self._.type
 
-  if self.bufnr then
-    if vim.api.nvim_buf_is_valid(self.bufnr) then
-      vim.api.nvim_buf_delete(self.bufnr, { force = true })
+  if type == "float" then
+    float_layout.unmount_box(self._.box)
+
+    if self.bufnr then
+      if vim.api.nvim_buf_is_valid(self.bufnr) then
+        vim.api.nvim_buf_delete(self.bufnr, { force = true })
+      end
+      self.bufnr = nil
     end
-    self.bufnr = nil
-  end
 
-  if vim.api.nvim_win_is_valid(self.winid) then
-    vim.api.nvim_win_close(self.winid, true)
+    if vim.api.nvim_win_is_valid(self.winid) then
+      vim.api.nvim_win_close(self.winid, true)
+    end
+    self.winid = nil
   end
-  self.winid = nil
 
   self._.loading = false
   self._.mounted = false
@@ -184,15 +211,20 @@ function Layout:update(config, box)
     config = {}
   end
 
-  u.update_layout_config(self._, config)
-
   if box then
     self._.box = Layout.Box(box)
+    self._.type = get_layout_type(self._.box)
   end
 
-  if self.winid then
-    vim.api.nvim_win_set_config(self.winid, self._.win_config)
-    self:_process_layout()
+  if self._.type == "float" then
+    local info = self._.float
+
+    u.update_layout_config(info, config)
+
+    if self.winid then
+      vim.api.nvim_win_set_config(self.winid, info.win_config)
+      self:_process_layout()
+    end
   end
 end
 
@@ -204,7 +236,17 @@ function Layout.Box(box, options)
   end
 
   if box.mount then
+    local type
+    if box:is_instance_of(Popup) then
+      type = "float"
+    end
+
+    if not type then
+      error("unsupported component")
+    end
+
     return {
+      type = type,
       component = box,
       grow = options.grow,
       size = options.size,
