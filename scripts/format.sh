c59bdcfde011b88bfb71b5c4351684cf67bf5f9f
Munif Tanjim <hello@muniftanjim.dev>
2022-10-15 16:34:03 +0600

    chore: fix scripts/format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -9,9 +9,9 @@ v_major="$(echo "${v}" | cut -d'.' -f1)"
 v_minor="$(echo "${v}" | cut -d'.' -f2)"
 v_patch="$(echo "${v}" | cut -d'.' -f3)"
 
-v_error_message="required stylua >= v${r_v_major}.${r_v_minor}.${r_v_patch}, found v${v_major}.${v_minor}.${v_patch}"
+v_error_message="required stylua ~v${r_v_major}.${r_v_minor}.${r_v_patch}, found v${v_major}.${v_minor}.${v_patch}"
 
-if test ${v_major} -ne ${r_v_major} || test ${v_minor} -lt ${r_v_minor} || test ${v_patch} -lt ${r_v_patch}; then
+if test ${v_major} -ne ${r_v_major} || test ${v_minor} -ne ${r_v_minor} || test ${v_patch} -lt ${r_v_patch}; then
   echo ${v_error_message} >&2
   exit 1
 fi
