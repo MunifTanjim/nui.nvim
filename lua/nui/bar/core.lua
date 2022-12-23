03f3ce7b342d5ee05c3f426d9342399f0a6b1ee5
Munif Tanjim <hello@muniftanjim.dev>
2022-12-23 15:32:00 +0600

    feat(bar): add some lower-level `core.add_*` functions

--- lua/nui/bar/core.lua
+++ lua/nui/bar/core.lua
@@ -130,83 +130,134 @@ end
 
 ---@param code string
 ---@param opts? nui_bar_core_item_options
+---@param parts string[]
+---@param parts_len? integer
+---@return integer parts_len
+function mod.add_code(code, opts, parts, parts_len)
+  local idx = parts_len or #parts
+
+  idx = idx + 1
+  parts[idx] = "%"
+
+  if opts then
+    if opts.align == "left" then
+      idx = idx + 1
+      parts[idx] = "-"
+    end
+
+    if opts.leading_zero then
+      idx = idx + 1
+      parts[idx] = "0"
+    end
+
+    if opts.min_width then
+      idx = idx + 1
+      parts[idx] = tostring(opts.min_width)
+    end
+
+    if opts.max_width then
+      idx = idx + 1
+      parts[idx] = "." .. opts.max_width
+    end
+  end
+
+  idx = idx + 1
+  parts[idx] = code
+
+  return idx
+end
+
+---@param code string
+---@param opts? nui_bar_core_item_options
 ---@return string item
 function mod.code(code, opts)
-  if not opts then
-    return "%" .. code
-  end
-
-  local parts = { "%" }
-
-  if opts.align == "left" then
-    parts[#parts + 1] = "-"
-  end
-
-  if opts.leading_zero then
-    parts[#parts + 1] = "0"
-  end
-
-  if opts.min_width then
-    parts[#parts + 1] = tostring(opts.min_width)
-  end
-
-  if opts.max_width then
-    parts[#parts + 1] = "." .. opts.max_width
-  end
-
-  parts[#parts + 1] = code
-
+  local parts = {}
+  mod.add_code(code, opts, parts, 0)
   return table.concat(parts)
+end
+
+---@param item string
+---@param opts { context?: nui_bar_core_item_options_context, id?: string, on_click: string|nui_bar_core_click_handler }
+---@param parts string[]
+---@param parts_len? integer
+---@return integer parts_len
+function mod.add_clickable(item, opts, parts, parts_len)
+  local idx = parts_len or #parts
+
+  local on_click = opts.on_click
+  local is_lua_fn = type(on_click) == "function"
+
+  parts[idx + 1] = "%"
+  parts[idx + 2] = is_lua_fn and store_fn(on_click, opts) or 0 --[[@as string]]
+  parts[idx + 3] = "@"
+  parts[idx + 4] = is_lua_fn and "v:lua.nui_bar_core_click_handler" or on_click --[[@as string]]
+  parts[idx + 5] = "@"
+  parts[idx + 6] = item
+  parts[idx + 7] = "%T"
+
+  return idx + 7
 end
 
 ---@param item string
 ---@param opts { context?: nui_bar_core_item_options_context, id?: string, on_click: string|nui_bar_core_click_handler }
 ---@return string clickable_item
 function mod.clickable(item, opts)
-  local on_click = opts.on_click
-  local is_lua_fn = type(on_click) == "function"
-
-  local parts = {
-    "%",
-    is_lua_fn and store_fn(on_click, opts) or 0,
-    "@",
-    is_lua_fn and "v:lua.nui_bar_core_click_handler" or on_click,
-    "@",
-    item,
-    "%T",
-  }
-
+  local parts = {}
+  mod.add_clickable(item, opts, parts, 0)
   return table.concat(parts)
+end
+
+---@param expression number|string|nui_bar_core_expression_fn
+---@param opts? nui_bar_core_expression_options
+---@param parts string[]
+---@param parts_len? integer
+---@return integer parts_len
+function mod.add_expression(expression, opts, parts, parts_len)
+  local idx = parts_len or #parts
+
+  local expand = opts and opts.expand
+
+  if expand then
+    idx = idx + 1
+    parts[idx] = "%{%"
+  else
+    idx = mod.add_code("{", opts, parts, idx)
+  end
+
+  if type(expression) == "function" then
+    idx = idx + 1
+    parts[idx] = "v:lua.nui_bar_core_expression_fn(" .. store_fn(expression, opts) .. ")"
+  elseif opts and opts.is_vimscript then
+    idx = idx + 1
+    parts[idx] = expression --[[@as string]]
+  else
+    idx = idx + 1
+    parts[idx] = "luaeval('" .. string.gsub(tostring(expression), "'", "''") .. "')"
+  end
+
+  idx = idx + 1
+  parts[idx] = expand and "%}" or "}"
+
+  return idx
 end
 
 ---@param expression number|string|nui_bar_core_expression_fn
 ---@param opts? nui_bar_core_expression_options
 ---@return string expression_item
 function mod.expression(expression, opts)
-  local expand = opts and opts.expand
-
-  ---@type (number|string)[]
-  local parts = {
-    expand and "%{%" or mod.code("{", opts),
-  }
-
-  if type(expression) == "function" then
-    parts[#parts + 1] = "v:lua.nui_bar_core_expression_fn(" .. store_fn(expression, opts) .. ")"
-  elseif opts and opts.is_vimscript then
-    parts[#parts + 1] = expression
-  else
-    parts[#parts + 1] = "luaeval('" .. string.gsub(tostring(expression), "'", "''") .. "')"
-  end
-
-  parts[#parts + 1] = expand and "%}" or "}"
-
+  local parts = {}
+  mod.add_expression(expression, opts, parts, 0)
   return table.concat(parts)
 end
 
 ---@param item number|string
 ---@param opts? nui_bar_core_item_options|{ tabnr?: integer, close?: boolean }
----@return string label_item
-function mod.label(item, opts)
+---@param parts string[]
+---@param parts_len? integer
+---@return integer parts_len
+function mod.add_label(item, opts, parts, parts_len)
+  local idx = parts_len or #parts
+
   if opts and opts.tabnr then
     local marker = "T"
     if opts.close then
@@ -216,36 +267,88 @@ function mod.label(item, opts)
       end
     end
 
-    return table.concat({
-      "%",
-      opts.tabnr,
-      marker,
-      item,
-      "%",
-      marker,
-    })
+    parts[idx + 1] = "%"
+    parts[idx + 2] = opts.tabnr --[[@as string]]
+    parts[idx + 3] = marker
+    parts[idx + 4] = item --[[@as string]]
+    parts[idx + 5] = "%"
+    parts[idx + 6] = marker
+
+    return idx + 6
   end
 
-  return mod.literal(item, opts)
+  return mod.add_literal(item, opts, parts, idx)
+end
+
+---@param item number|string
+---@param opts? nui_bar_core_item_options|{ tabnr?: integer, close?: boolean }
+---@return string label_item
+function mod.label(item, opts)
+  local parts = {}
+  mod.add_label(item, opts, parts, 0)
+  return table.concat(parts)
+end
+
+---@param item boolean|number|string
+---@param opts? nui_bar_core_item_options
+---@param parts string[]
+---@param parts_len? integer
+---@return integer parts_len
+function mod.add_literal(item, opts, parts, parts_len)
+  local idx = parts_len or #parts
+
+  if not opts then
+    idx = idx + 1
+    parts[idx] = string.gsub(tostring(item), "%%", "%%%%")
+    return idx
+  end
+
+  opts.is_vimscript = true
+  return mod.add_expression("'" .. string.gsub(tostring(item), "'", "''") .. "'", opts, parts, idx)
 end
 
 ---@param item boolean|number|string
 ---@param opts? nui_bar_core_item_options
 ---@return string literal_item
 function mod.literal(item, opts)
-  if not opts then
-    return (string.gsub(tostring(item), "%%", "%%%%"))
+  local parts = {}
+  mod.add_literal(item, opts, parts, 0)
+  return table.concat(parts)
+end
+
+---@param items string|string[]
+---@param opts? nui_bar_core_item_options
+---@param parts string[]
+---@param parts_len? integer
+---@return integer parts_len
+function mod.add_group(items, opts, parts, parts_len)
+  local idx = parts_len or #parts
+
+  idx = mod.add_code("(", opts, parts, idx)
+
+  if type(items) == "table" then
+    for i = 1, #items do
+      idx = idx + 1
+      parts[idx] = items[i]
+    end
+  else
+    idx = idx + 1
+    parts[idx] = items
   end
 
-  opts.is_vimscript = true
-  return mod.expression("'" .. string.gsub(tostring(item), "'", "''") .. "'", opts)
+  idx = idx + 1
+  parts[idx] = "%)"
+
+  return idx
 end
 
 ---@param items string|string[]
 ---@param opts? nui_bar_core_item_options
 ---@return string grouped_items
 function mod.group(items, opts)
-  return mod.code("(", opts) .. (type(items) == "table" and table.concat(items) or items) .. "%)"
+  local parts = {}
+  mod.add_group(items, opts, parts, 0)
+  return table.concat(parts)
 end
 
 -- `highlight` can be:
